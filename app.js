a511668b9ce641056f6f918e0cec01fe3b0785bf
Acheev Bhagat <Acheev Bhagat>
2016-11-05 02:15:42 -0400

    seeing which dependencies work

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+var twitter = require('twitter');
+var config = {
+    consumer_key: 'goBgEALwjFShpLj7ULG3mTfEB',
+    consumer_secret: 'PZE9uFExwunffVdpbNdayXIkLzUbZaauyFXnE0H209yQmwQam2',
+    access_token_key: '3041579079-FWv7W4hjviZGWliohmK1NqkabttRamFWlkttVjs',
+    access_token_secret: 'yeFFxBI2toLUqvkfJn3ijYujvXzmP6W7FLLKvd7dN99aw'
+}
+
+var twitterClient = new twitter(config);
